cd3e60d432ad85e462e1d8104402989884dbf426
melihuludag <69352117+melihuludag@users.noreply.github.com>
2021-07-08 05:11:39 +0300

    Update Question 4-5-6.py

--- Question 4-5-6.py
+++ Question 4-5-6.py
@@ -1,31 +1,31 @@
-
+import pandas as pd
+url ="https://raw.githubusercontent.com/melihuludag/Technical-Questionnaire-/main/country_vaccination_stats.csv"
+df = pd.read_csv(url)
+print(df)
 
 '''
 Q U E S T I O N 4
 
-Code Implementation Task: Implement code to fill the missing data (impute) in daily_vaccinations column per country with the minimum daily vaccination number of relevant countries.
+Code Implementation Task: Implement code to fill the missing data (impute) in daily_vaccinations column per country with the minimum daily vaccination number of relevant countries.  
 Note: If a country does not have any valid vaccination number yet, fill it with “0” (zero). 
+Please  provide the link to your code as answer to this question.
 '''
 
-# ACCESS CVS FILE
-
-import pandas as pd
-import numpy as np
-df = pd.read_csv('country_vaccination_stats.csv')
-print(df)
-
 #Except of groups that does not have any value, null parts of all group is filled by min values of groups.
-''-----------------------------------------------------------------------------------------------------------
 dp = df.groupby('country')['daily_vaccinations']
 df['daily_vaccinations'] = df['daily_vaccinations'].fillna(dp.transform('min'))
 
-''-----------------------------------------------------------------------------------------------------------
+
 
 #Remain rows' null cells are filled by 0 because of they do not have any values in column daily_vaccinations.
-''-----------------------------------------------------------------------------------------------------------
 dp = df.groupby('country')['daily_vaccinations']
+
 df['daily_vaccinations'].fillna(0, inplace = True)
-''-----------------------------------------------------------------------------------------------------------
+print("A N S W E R     O F    Q U E S T I O N 4")
+print(df.head(3))
+for header in df.columns.values.tolist():
+    print("Number of null elements of column '{}' is  {}".format(header,pd.isnull(df[header]).sum()) )
+print("\n\n")
 
 
 '''
@@ -36,8 +36,9 @@ numbers by considering missing values imputed version of dataset.
 '''
 
 a = df.groupby('country')['daily_vaccinations'].median().reset_index()
-a.sort_values(by='daily_vaccinations', ascending=False).head(3)
-
+print("A N S W E R     O F    Q U E S T I O N 5")
+print(a.sort_values(by='daily_vaccinations', ascending=False).head(3))
+print("\n\n")
 
 '''
 Q U E S T I O N 6
@@ -47,4 +48,5 @@ What is the number of total vaccinations done on 1/6/2021 (MM/DD/YYYY) by consid
 
 b = df.groupby('date')['daily_vaccinations']
 b.get_group("1/6/2021").sum()
-
+print("A N S W E R     O F    Q U E S T I O N 6")
+print(b.get_group("1/6/2021").sum())
