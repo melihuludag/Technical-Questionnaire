3879a0ba508574f738310f4c486c05477d504591
melihuludag <69352117+melihuludag@users.noreply.github.com>
2021-07-08 04:36:25 +0300

    Update Question 4.py

--- Question 4.py
+++ Question 4.py
@@ -3,9 +3,29 @@ Code Implementation Task: Implement code to fill the missing data (impute) in da
 Note: If a country does not have any valid vaccination number yet, fill it with “0” (zero). 
 '''
 
+# ACCESS CVS FILE
+
 import pandas as pd
 import numpy as np
 df = pd.read_csv('country_vaccination_stats.csv')
 print(df)
+
+#Except of groups that does not have any value, null parts of all group is filled by min values of groups.
+'-----------------------------------------------------------------------------------------------------------
+dp = df.groupby('country')['daily_vaccinations']
+df['daily_vaccinations'] = df['daily_vaccinations'].fillna(dp.transform('min'))
+
+'-----------------------------------------------------------------------------------------------------------
+
+#Remain rows' null cells are filled by 0 because of they do not have any values in column daily_vaccinations.
+'-----------------------------------------------------------------------------------------------------------
+dp = df.groupby('country')['daily_vaccinations']
+df['daily_vaccinations'].fillna(0, inplace = True)
+'-----------------------------------------------------------------------------------------------------------
+
+
+
+
+
 
 
